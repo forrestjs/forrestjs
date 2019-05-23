520b92ad9d951d4a98ef3db277f8b8835a92b3c4
Marco Pegoraro <marco.pegoraro@gmail.com>
2019-05-23 10:44:31 +0200

    Add package-lock.json for circleci

---
+++ packages/cli/__test__/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
+
+// const module = require(`../`)
+describe(`@forrestjs/cli`, () => {
+  it(`should work`,
+  /*#__PURE__*/
+  (0, _asyncToGenerator2.default)(function* () {// console.log('it does')
+  }));
+});
