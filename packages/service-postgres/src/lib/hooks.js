d8a70d2bb28f65394378943bf626c8f99a9c84a9
Marco Pegoraro <marco.pegoraro@gmail.com>
2019-12-18 15:15:49 +0100

    Fix service postgres name

--- packages/service-postgres/src/lib/hooks.js
+++ packages/service-postgres/src/lib/hooks.js
@@ -1,5 +1,5 @@
 import { SERVICE } from '@forrestjs/hooks'
-export const SERVICE_NAME = `${SERVICE} posrgres`
+export const SERVICE_NAME = `${SERVICE} postgres`
 
 export const POSTGRES_BEFORE_INIT = `${SERVICE_NAME}/beforeInit`
 export const POSTGRES_BEFORE_START = `${SERVICE_NAME}/beforeStart`
