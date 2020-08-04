0d0c0d36bcd2c98a5cf4c9f3683a46f1d067c114
marco pegoraro <marco.pegoraro@gmail.com>
2020-08-04 17:02:14 +0200

    Improved fastify healthz hooks API

--- packages/service-fastify-healthz/CHANGELOG.md
+++ packages/service-fastify-healthz/CHANGELOG.md
@@ -1,2 +1,24 @@
 # Change Log
 
+## v1.13.2
+
+Allows to return the route handler from the hooks' hander
+
+```js
+const featureCustomHealthz = [
+  "$FASTIFY_HEALTHZ_HANDLER",
+  () => (req, res) => res.send("ok - custom handler")
+];
+```
+
+is the shorthand for: 
+
+```js
+const featureCustomHealthz = [
+  "$FASTIFY_HEALTHZ_HANDLER",
+  ({ registerHandler }) => registerHandler((req, res) => res.send("ok - custom handler"))
+];
+```
+
+**NOTE:** Only the first registered hook will be used and the utilization of `registerHandler`
+API will overrides any shorthand returned value.
--- packages/service-fastify-healthz/src/lib/index.js
+++ packages/service-fastify-healthz/src/lib/index.js
@@ -16,7 +16,12 @@ module.exports = ({ registerAction, createHook, getConfig, registerHook }) => {
       // Let a custom handler logic being injected
       let customHandler = null;
       const registerHandler = (handler) => (customHandler = handler);
-      createHook.sync(hooks.FASTIFY_HEALTHZ_HANDLER, { registerHandler });
+      const handlers = createHook.sync(hooks.FASTIFY_HEALTHZ_HANDLER, { registerHandler });
+
+      // Allows to return the route handler from the hooks' hander
+      if (!customHandler && handlers.length && typeof handlers[0][0] === 'function') {
+        customHandler = handlers[0][0]
+      }
 
       // Setup the route
       registerRoute({
