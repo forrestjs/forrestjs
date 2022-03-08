b89729bc27157458f1b48c5073275b122bfa2041
marcopeg <marco.pegoraro@gmail.com>
2022-03-08 15:46:32 +0100

    Move jest globals

--- packages/service-pg/test/globals.js
+++ packages/service-pg/test/globals.js
@@ -1,5 +1,5 @@
 console.warn(
-  'DEPRECATED: use the same package from "/src/jest-globals" instead',
+  'DEPRECATED: use the same package from "/jest/globals" instead',
 ).module.exports = (global) => {
   const query = (q, p) => global.post('/test/pg/query', { q, p });
 
