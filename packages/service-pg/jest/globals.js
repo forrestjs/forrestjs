03e941bc4671ebdf2efd05412a4855a7cb1169b9
marcopeg <marco.pegoraro@gmail.com>
2022-03-08 15:52:17 +0100

    Fix packages

--- packages/service-pg/jest/globals.js
+++
@@ -1,9 +0,0 @@
-module.exports = (global) => {
-  const query = (q, p) => global.post('/test/pg/query', { q, p });
-
-  return {
-    pg: {
-      query,
-    },
-  };
-};
--- packages/service-pg/test/globals.js
+++ packages/service-pg/test/globals.js
@@ -1,6 +1,4 @@
-console.warn(
-  'DEPRECATED: use the same package from "/jest/globals" instead',
-).module.exports = (global) => {
+module.exports = (global) => {
   const query = (q, p) => global.post('/test/pg/query', { q, p });
 
   return {
